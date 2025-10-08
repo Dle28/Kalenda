b1d97787becf13b167a6270a71ffedeb190a5fbf
DungLee28 <dung.levc2810@gmail.com>
2025-10-09 02:10:09 +0700

    Update anchorClient.ts

--- apps/web/src/lib/anchorClient.ts
+++ apps/web/src/lib/anchorClient.ts
@@ -5,5 +5,5 @@ import { myProgramIdl as idl } from "@starter/ts-sdk/idl";
 export const getProgram = (conn: Connection, wallet: any) => {
   const provider = new AnchorProvider(conn, wallet, { commitment: "processed" });
   const programId = new PublicKey(process.env.NEXT_PUBLIC_PROGRAM_ID!);
-  return new Program(idl as unknown as Idl, provider, programId);
+  return new Program(idl as unknown as Idl, programId, provider);
 };
