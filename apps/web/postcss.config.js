3ec5c20840804c03035d96c3201b1352008a6144
DungLee28 <dung.levc2810@gmail.com>
2025-10-16 02:59:13 +0700

    up

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,6 +1,5 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+// Next.js expects a CommonJS export for postcss.config.js
+// Keep it minimal since we don't use Tailwind here.
+module.exports = {
+  plugins: {},
 };
