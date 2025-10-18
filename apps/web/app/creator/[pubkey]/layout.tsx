1d4e0aded9cf8788434a3f0c3f6352835d9eb3c5
DungLee28 <dung.levc2810@gmail.com>
2025-10-18 14:01:04 +0700

    up

---
+++ apps/web/app/creator/[pubkey]/layout.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../profile.css';
+
+export default function CreatorProfileLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
---
+++ apps/web/app/slot/layout.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './styles.css';
+
+export default function SlotLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
