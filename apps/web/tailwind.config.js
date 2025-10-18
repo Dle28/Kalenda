1afecc9891bddc70afc291ecdcb731ecd316871e
DungLee28 <dung.levc2810@gmail.com>
2025-10-18 13:56:01 +0700

    up

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}"
+  ],
   theme: { extend: {} },
   plugins: [],
 };
